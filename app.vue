c6997ba9a15d83485d0a9db09ac8f5d0e14e5b55
y.zharenkov <y.zharenkov@skladno.com>
2025-07-18 14:06:18 +0300

    feat: add pages

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <span>Hello</span>
+<!--    <NuxtRouteAnnouncer />-->
+<!--    <NuxtWelcome />-->
   </div>
 </template>
+
+<script setup lang="ts">
+import "~/assets/styles/main.css"
+const counter = ref(0)
+console.log('Setup')
+onMounted(() => {
+  console.log('onMounted')
+})
+
+</script>
---
+++ pages/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    edit\create post
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
